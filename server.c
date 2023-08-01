619a303a230452196bf263daf1ac73d8e1c693c3
pranavathreya <pranavsubbu@gmail.com>
2023-08-01 12:58:39 -0400

    rewrote server from scratch

--- server.c
+++ server.c
@@ -1,97 +1,79 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/socket.h>
+#include <stdlib.h> 
 #include <netdb.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/socket.h>
 
 #define BUF_SIZE 500
 
-int main(int argc, char* argv[])
+int setupServer(char* port)
 {
 	struct addrinfo hints;
 	struct addrinfo *result, *rp;
+	int s, sfd;
 	
-	int sfd, s;
-	struct sockaddr_storage peerinfo;
-	socklen_t peeraddr_len;
-	ssize_t nread;
-
-	char buf[BUF_SIZE];
-
-	if (argc!=2)
-	{
-		fprintf(stderr, "server: Usage: %s port\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_protocol = 0;
-	hints.ai_addr = NULL;
 	hints.ai_canonname = NULL;
-	hints.ai_next = NULL;
+	hints.ai_addr = NULL;
+	hints.ai_addrlen = 0;
+	
+	s = getaddrinfo(NULL, port, &hints,
+			&result);
 
-	s = getaddrinfo(NULL, argv[1], &hints, &result);
-	if (s != 0)
+	if (s == -1)
 	{
-		fprintf(stderr, "server: getaddrinfo failed bitch: %s\n", gai_strerror(s));
+		fprintf(stderr, "server: getaddrinfo failed: %s\n",
+			gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
 
-	for (rp = result; rp != NULL; rp = rp->ai_next)
-	{	
-		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+	for (rp=result; rp!=NULL; 
+			rp=rp->ai_next)
+	{
+		sfd = socket(rp->ai_family, rp->ai_socktype,
+			       	rp->ai_protocol);
+		
 		if (sfd == -1)
 			continue;
 
-		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
-			break; /* Success */
-		close(sfd);
+		if (bind(sfd, rp->ai_addr,
+				       rp->ai_addrlen) == 0)
+			break;	
 	}
 
 	freeaddrinfo(result);
-	
+
 	if (rp == NULL)
 	{
-		fprintf(stderr, "server: binding failed bitch\n");
+		fprintf(stderr, "server: Failed to bind.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	
-	char host[NI_MAXHOST], service[NI_MAXSERV];
-	getnameinfo((struct sockaddr*) rp->ai_addr, rp->ai_addrlen,
+	char host[NI_MAXHOST], serv[NI_MAXSERV];
+
+	getnameinfo(rp->ai_addr, rp->ai_addrlen,
 			host, NI_MAXHOST,
-			service, NI_MAXSERV, 0);
-	fprintf(stdout, "server: Successfully bound running on: %s:%s\n", host, service);
-		
-	for (;;)
-	{
-		peeraddr_len = sizeof(peerinfo);
-		memset(buf, 0, BUF_SIZE);
-		nread = recvfrom(sfd, buf, BUF_SIZE, 0,
-			       (struct sockaddr*) &peerinfo, &peeraddr_len);
-
-		if (nread == -1)
-		{
-			fprintf(stderr, "server: could not read from peer bitch: %s\n", gai_strerror(nread));
-			exit(EXIT_FAILURE);
-		}
-
-		getnameinfo((struct sockaddr*) &peerinfo, peeraddr_len,
-			       host, NI_MAXHOST,
-				service, NI_MAXSERV, 0);
-
-		fprintf(stdout, "server: Received %s bytes from %s:%s bitch.\n",
-				buf, host, service);
-
-		if (sendto(sfd, buf, BUF_SIZE, 0, (struct sockaddr*) &peerinfo, peeraddr_len) < -1)
-		{
-			fprintf(stderr, "server: failed to send to client: %s:%s\n", host, service);
-			exit(EXIT_FAILURE);
-		}
-
-	}		
-	return 0;
+			serv, NI_MAXSERV, 0);
+	fprintf(stdout, "server: successfully bound to: %s:%s\n", host, port);
+	return sfd;
 }
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s port\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int sfd;
+	char buf[BUF_SIZE];
+	struct sockaddr peerAddress;
+	size_t peerAddressLen;
+
+	sfd = setupServer(argv[1]);
+}
+
+
+
